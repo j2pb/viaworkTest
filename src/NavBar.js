b4fc28f6823475662385ef9e14047bfc064db58f
Juan Pablo <juanpablo@Tevis-iMac.local>
2018-12-11 18:12:18 -0600

    Se agregan badges en iconos.

--- src/NavBar.js
+++ src/NavBar.js
@@ -9,44 +9,50 @@ class NavBar extends Component {
             <header className="App-header">
                 <nav className="navbar navbar-expand-lg bg-light">
                     <a className="navbar-brand" href="#">Start Bootstrap</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div className="collapse navbar-collapse" id="navbarText">
                         <form className="form-inline my-2 my-lg-0 ml-auto">
                             <div className="input-group">
-
                                 <input type="text" className="form-control searchbox" aria-label="" placeholder="Search for..." />
+
                                 <div className="input-group-append">
                                     <span className="input-group-text">
                                         <FontAwesomeIcon icon={faSearch} />
                                     </span>
                                 </div>
+                                <div className="autocomplete">
+
+                                </div>
                             </div>
                         </form>
                         <ul className="navbar-nav">
                             <li className="nav-item">
-
                                 <a className="nav-link" href="#">
-                                    <FontAwesomeIcon icon={faBell} />
+                                    <span className="fa-layers fa-fw">
+                                        <FontAwesomeIcon className="fas fa-lg" icon={faBell} />
+                                        <span className="fa-layers-counter">9+</span>
+                                    </span>
                                 </a>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="#">
-                                    <FontAwesomeIcon icon={faEnvelope} />
+                                    <span className="fa-layers fa-fw">
+                                        <FontAwesomeIcon className="fas fa-lg" icon={faEnvelope} />
+                                        <span className="fa-layers-counter">7</span>
+                                    </span>
                                 </a>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="#">
-                                    <FontAwesomeIcon icon={faUserCircle} />
+                                    <span className="fa-layers fa-fw">
+                                        <FontAwesomeIcon className="fas fa-lg" icon={faUserCircle} />
+                                    </span>
                                 </a>
                             </li>
-
                         </ul>
-
-
-
                     </div>
                 </nav>
             </header>
